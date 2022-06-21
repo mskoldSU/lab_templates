abb2dd8c5143db7e7a736c8c1081170144d37d3e
Elias Lundell <elilun03@gmail.com>
2022-06-21 09:31:09 +0200

    Remove git

--- auto.template.creation/dev/01_start.R
+++ auto.template.creation/dev/01_start.R
@@ -42,7 +42,7 @@ usethis::use_lifecycle_badge("Experimental")
 usethis::use_news_md(open = FALSE)
 
 ## Use git ----
-usethis::use_git()
+# usethis::use_git()
 
 ## Init Testing Infrastructure ----
 ## Create a template for tests
