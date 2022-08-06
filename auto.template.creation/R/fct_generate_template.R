5f2de8079f1884fba53e24c75a8e41b5ecdf815d
Elias Lundell <elilun03@gmail.com>
2022-08-06 21:30:51 +0200

    Remove function header based on old assumption of data to be entered

--- auto.template.creation/R/fct_generate_template.R
+++ auto.template.creation/R/fct_generate_template.R
@@ -5,6 +5,6 @@
 #' @return The return value, if any, from executing the function.
 #'
 #' @noRd
-generate_template <- function(cols_df, order_df, start_accnr_df, start_provid_df, sheets) {
+generate_template <- function() {
 
 }
